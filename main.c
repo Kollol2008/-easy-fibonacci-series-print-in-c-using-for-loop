a79c80aa488e606fc59e91205db1492d652afa30
Ashikur Zaman Kollol <zamankollol12@gmail.com>
2025-02-27 23:02:56 +0600

    main.c

--- main.c
+++ main.c
@@ -9,9 +9,7 @@ int main()
         printf("%d ",first);  //first it'll print 0
 
         int fibo;
-        fibo=first+second;  /*it'll do addition of first and second number.
-                            whenever the loop execute first time
-                            and contain the value in fibo*/
+        fibo=first+second;  /*it'll do addition of first and second number.whenever the loop execute first time and contain the value in fibo*/
         first=second;
         second=fibo;
      }
